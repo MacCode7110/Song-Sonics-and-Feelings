b3ea59d8524672b4eb839e443596c3003151892a
Matthew <memcalarney@wpi.edu>
2026-07-09 21:01:46 -0400

    Correct Layout component import naming in App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './components/layout'
+import Layout from './components/Layout'
 import InteractivePCA from './pages/InteractivePCA'
 import DataMethodology from './pages/DataMethodology'
 
