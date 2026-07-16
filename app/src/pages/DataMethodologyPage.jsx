abc66d1d513083f4c89aea05f0eaa453cc40c946
Matthew <memcalarney@wpi.edu>
2026-07-15 21:26:08 -0400

    Correct spelling and grammar issues in DataMethodologyPage.jsx

--- app/src/pages/DataMethodologyPage.jsx
+++ app/src/pages/DataMethodologyPage.jsx
@@ -29,23 +29,23 @@ const DataMethodologyPage = () => {
         </header>
         <main className="block mb-1">
           <Content size={5} className="is-family-secondary has-text-black p-5 mb-1">
-            <span className="has-text-weight-semibold">1. Initial Quality Sweep, Manual Context Review, Establish structural base</span>: <span className="is-italic">survey_data_master_raw.csv</span>{' '}
-            was refactored to only contain <span className="is-italic">[song_name]</span>, <span className="is=italic">[artist_name]</span>, and <span className="is-italic">[primary_feeling]</span>{' '}
+            <span className="has-text-weight-semibold">1. Initial Quality Sweep, Manual Context Review, Establishing structural base</span>: <span className="is-italic">survey_data_master_raw.csv</span>{' '}
+            was refactored to only contain <span className="is-italic">[song_name]</span>, <span className="is-italic">[artist_name]</span>, and <span className="is-italic">[primary_feeling]</span>{' '}
             columns. Additionally, all null values were standardized to display NA. The results were recorded to <span className="is-italic">survey_data_master_structural_base.csv</span>.
           </Content>
           <Content size={5} className="is-family-secondary has-text-black p-5 mb-1">
             <span className="has-text-weight-semibold">
-              2. Dataset Truncation (Simple Random Sampling), <span className="is-italic">Python</span> Program Execution, Select representative subset of dataset
+              2. Dataset Truncation (Simple Random Sampling), <span className="is-italic">Python</span> Program Execution, Selecting representative subset of dataset
             </span>
             : To maintain an efficient data cleaning workflow and provide an unbiased, representative subset of the full-time employee respondent pool, a simple random sampling method was applied.
-            Executing <span className="is-italic">simple_random_sampling.py</span> on <span className="is-italic">survey_data_master_structural_base.csv</span> with a constant random seed
-            (random_state=75) to enforce reproducibility, 85 unique survey responses were randomly selected from the 275 data rows following the{' '}
+            By executing <span className="is-italic">simple_random_sampling.py</span> on <span className="is-italic">survey_data_master_structural_base.csv</span> with a constant random seed
+            (random_state=75) to enforce reproducibility, we randomly selected 85 unique survey responses from the 275 data rows following the{' '}
             <span className="is-italic">Initial Quality Sweep</span> in Item 1. The sample size represents approximately 30.9% of the full-time employee respondent pool, which asserts a 95% confidence
             level and a margin of error less than 9%. All non-selected responses were omitted from this phase of analysis. The results were recorded to{' '}
             <span className="is-italic">survey_data_master_sampled.csv</span>.
           </Content>
           <Content size={5} className="is-family-secondary has-text-black p-5 mb-1">
-            <span className="has-text-weight-semibold">3. Complete Quality Sweep and Song URL Insertion, Manual Context Review, Remove invalid data and systematically correct information</span>: While
+            <span className="has-text-weight-semibold">3. Complete Quality Sweep and Song URL Insertion, Manual Context Review, Removing invalid data and systematically correcting information</span>: While
             the 85 data rows sampled during the <span className="is-italic">Dataset Truncation (Simple Random Sampling)</span> in Item 2 established a 95% confidence level and margin of error less
             than 9% for the full-time employee pool, the domain constraints enforced in Item 3 decreased the usable data rows to 45. The resulting <span className="is-italic">PCA</span> operates as an
             exploratory subset of the 85 sampled data rows. The domain constraints detailed in sections <span className="is-italic">Data Correction Measures</span>,{' '}
@@ -55,7 +55,7 @@ const DataMethodologyPage = () => {
           </Content>
           <Content size={5} className="is-family-secondary has-text-black p-5 mb-1">
             <span className="has-text-weight-semibold">
-              4. Primary Feeling Mapping, <span className="is-italic">Python</span> Program Execution, Map each remaining primary feeling in the <span className="is-italic">[primary_feeling]</span>{' '}
+              4. Primary Feeling Mapping, <span className="is-italic">Python</span> Program Execution, Mapping each remaining primary feeling in the <span className="is-italic">[primary_feeling]</span>{' '}
               column to one of the four quadrants established in <span className="is-italic">Russell's Core Affect Framework</span>
             </span>
             : To provide a method for understanding the creation of feelings in comparison to sonic features extracted from <span className="is-italic">Essentia</span>, executing{' '}
@@ -71,8 +71,8 @@ const DataMethodologyPage = () => {
           </Content>
           <Content size={5} className="is-family-secondary has-text-black p-5 mb-1">
             <span className="has-text-weight-semibold">
-              5. Song Downloading and <span className="is-italic">WAV</span> Conversion, <span className="is-italic">Python</span> Program Execution, Download each song in the{' '}
-              <span className="is-italic">[song_name]</span> column through the corresponding URL in the <span className="is-italic">[youtube_music_url]</span> column and convert to{' '}
+              5. Song Downloading and <span className="is-italic">WAV</span> Conversion, <span className="is-italic">Python</span> Program Execution, Downloading each song in the{' '}
+              <span className="is-italic">[song_name]</span> column through the corresponding URL in the <span className="is-italic">[youtube_music_url]</span> column and converting to{' '}
               <span className="is-italic">WAV</span>
             </span>
             : To build a standardized audio collection for sonic feature extraction from <span className="is-italic">Essentia</span>, executing <span className="is-italic">wav_file_creation.py</span>{' '}
@@ -84,7 +84,7 @@ const DataMethodologyPage = () => {
           </Content>
           <Content size={5} className="is-family-secondary has-text-black p-5 mb-1">
             <span className="has-text-weight-semibold">
-              6. <span className="is-italic">Essentia</span> Sonic Feature Extraction, <span className="is-italic">Python</span> Program Execution, Calculate and record 12 sonic scalar values for each
+              6. <span className="is-italic">Essentia</span> Sonic Feature Extraction, <span className="is-italic">Python</span> Program Execution, Calculating and recording 12 sonic scalar values for each
               of the 45 remaining data rows through accessing the <span className="is-italic">WAV</span> file referenced in the <span className="is-italic">[wav_filename]</span> column
             </span>
             : To build a collection of sonic features extracted from <span className="is-italic">Essentia</span>, executing <span className="is-italic">sonic_feature_extraction.py</span> on{' '}
@@ -94,17 +94,17 @@ const DataMethodologyPage = () => {
           </Content>
           <Content size={5} className="is-family-secondary has-text-black p-5 mb-1">
             <span className="has-text-weight-semibold">
-              7. Sonic Feature Scalar Standardization, <span className="is-italic">Python</span> Program Execution, Standardize the 12 sonic scalar values for each of the 45 remaining data rows
+              7. Sonic Feature Scalar Standardization, <span className="is-italic">Python</span> Program Execution, Standardizing the 12 sonic scalar values for each of the 45 remaining data rows
             </span>
             : To prepare the 12 sonic scalar values for correct rendering on the <span className="is-italic">PCA</span>, executing <span className="is-italic">sonic_feature_standardization.py</span>{' '}
             on <span className="is-italic">survey_data_master_sonic_feature_calculations.csv</span> looped through the 45 remaining data rows and applied a standardization operation to each of the
-            sonic scalar values. The operation utilized a z-score normalization to transform each of the 12 sonic scalar values assigning a mean of 0 and a standard deviation of 1. Since the{' '}
+            sonic scalar values. The operation utilized a z-score normalization to transform each of the 12 sonic scalar values, assigning a mean of 0 and a standard deviation of 1. Since the{' '}
             <span className="is-italic">PCA</span> calculates variance according to magnitude, the geometric relationships between points are accurately presented. The results were recorded to{' '}
             <span className="is-italic">survey_data_master_sonic_feature_standardization.csv</span>.
           </Content>
           <Content size={5} className="is-family-secondary has-text-black p-5 mb-1">
             <span className="has-text-weight-semibold">
-              8. <span className="is-italic">PCA</span> Dimensionality Reduction, <span className="is-italic">Python</span> Program Execution, Compress 12 multi-dimensional standardized sonic features
+              8. <span className="is-italic">PCA</span> Dimensionality Reduction, <span className="is-italic">Python</span> Program Execution, Compressing 12 multi-dimensional standardized sonic features
               into 2 static spatial dimensions (<span className="is-italic">pca_x</span> and <span className="is-italic">pca_y</span>) for <span className="is-italic">PCA</span> rendering
             </span>
             : To map multi-dimensional standardized sonic features onto a 2D scatter plot area, executing <span className="is-italic">calculate_pca_coordinates.py</span> on{' '}
