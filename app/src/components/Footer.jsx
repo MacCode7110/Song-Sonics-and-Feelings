fcace52b94b612136aa323a7a399789f961cc458
Matthew <memcalarney@wpi.edu>
2026-07-16 23:22:05 -0400

    Update footer description for clarity on project purpose

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -2,8 +2,7 @@ const Footer = () => {
   return (
     <footer className="footer has-background-success-light">
       <div className="content has-text-centered has-text-weight-medium has-text-black is-size-5">
-        <strong>Matthew McAlarney</strong> (Web Developer, Data Researcher, Scratch DJ, Rapper) developed <em>Song Sonics and Feelings</em> to understand the relationship between music
-        preferences and the construction of feelings.
+        <strong>Matthew McAlarney</strong> (Web Developer, Data Researcher, Scratch DJ, Rapper) developed <em>Song Sonics and Feelings</em> to understand the connection between the sonic qualities of songs and the resulting emotional experience of listeners.
       </div>
     </footer>
   )
